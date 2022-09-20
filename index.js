79c0a9af9ce4da74ced75b8db74d17ec10b7ad2a
Masuduzzaman Mohammad <masuduzzaman_811@outlook.com>
2022-09-20 13:47:51 +0600

    add the solution and readme file

---
+++ index.js
@@ -0,0 +1,16 @@
+function getMoneySpent(keyboards, drives, b) {
+  /*
+   * Write your code here.
+   */
+  let  expensive = BigInt(-1);
+  for(let i = 0; i < keyboards.length; ++i){
+      for(let j= 0; j < drives.length; ++j){
+          const exp = BigInt(keyboards[i]) + BigInt(drives[j]);
+          if( (exp < BigInt(b)) && (exp > expensive)){
+              expensive = exp;
+          }
+      }
+  }
+  return expensive;
+
+}
